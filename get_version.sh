2ea7fc9e5305b988851fe40694430e2554ae6109
reese sapphire <reese@ovine.xyz>
2023-05-21 16:48:56 -0500

    make commit hash in version string conditional. include tags in git pull step.

--- get_version.sh
+++ get_version.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-tag="v$(jq -r '.version' package.json)"
 git config --global --add safe.directory "$(pwd)"
+
+tag="v$(jq -r '.version' package.json)"
 if git describe --tags --abbrev=0 > /dev/null 2>&1; then
 	tag="$(git describe --tags --abbrev=0)"
 fi
+
+tag_commit="$(git rev-parse --short "$tag")"
+cur_commit="$(git rev-parse --short HEAD)"
+
+commit="$([ "$tag_commit" != "$cur_commit" ] && echo "+$cur_commit")"
+
 status="$([ "$(git status --porcelain | wc -l)" -gt 0 ] && echo "-dirty")"
-version="$tag+$(git rev-parse --short HEAD)${status}"
+version="$tag${commit}${status}"
 
 echo "export default \"${version}\""
