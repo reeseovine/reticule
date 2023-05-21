0bfb1e22d534c431f4a17caaf3b7b83bc60d26df
reese sapphire <reese@ovine.xyz>
2023-05-21 15:01:51 -0500

    .editorconfig

--- get_version.sh
+++ get_version.sh
@@ -5,7 +5,7 @@ git config --global --add safe.directory "$(pwd)"
 if git describe --tags --abbrev=0 > /dev/null 2>&1; then
 	tag="$(git describe --tags --abbrev=0)"
 fi
-status=$([ "$(git status --porcelain | wc -l)" -gt 0 ] && echo "-dirty")
+status="$([ "$(git status --porcelain | wc -l)" -gt 0 ] && echo "-dirty")"
 version="$tag+$(git rev-parse --short HEAD)${status}"
 
 echo "export default \"${version}\""
